8e069fb3b96f31f600b4d20a28fac30a765571f0
niclasdsgmbh <108051793+niclasdsgmbh@users.noreply.github.com>
2022-10-31 00:19:20 +0100

    Update manager.sh

--- manager.sh
+++ manager.sh
@@ -3,3 +3,5 @@ kubeadm init --control-plane-endpoint=manager
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f /git/kube-flannel.yml
