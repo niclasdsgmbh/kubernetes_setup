26cca980d06199eda53fc997ce39c04a97e4c9e9
niclasdsgmbh <108051793+niclasdsgmbh@users.noreply.github.com>
2022-10-30 15:14:26 +0100

    Update manager.sh

--- manager.sh
+++ manager.sh
@@ -1,5 +1,5 @@
 # INIT KUBEADM
-kubeadm init --control-plane-endpoint=master
+kubeadm init --control-plane-endpoint=manager
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
