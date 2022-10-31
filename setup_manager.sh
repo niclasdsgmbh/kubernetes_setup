7dc5307b62892e97652874c928174ddbf7421f23
niclasdsgmbh <108051793+niclasdsgmbh@users.noreply.github.com>
2022-10-31 22:04:27 +0100

    update

--- setup_manager.sh
+++ setup_manager.sh
@@ -110,10 +110,29 @@ kubectl apply -f /git/storageclass.yaml
 kubectl patch storageclass gluster -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
 ##############################
-# AUTOCLEAN                  #
+# INSTALL DOCKER             #
 ##############################
 
-apt autoclean -y
+apt-get update 
+apt-get upgrade -y 
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common \
+    unattended-upgrades
+    
+rm -f /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
+
+apt-get update
+apt-get install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-compose-plugin
 
 ##############################
 # DEPLOY MINIO               #
