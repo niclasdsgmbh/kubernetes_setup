daf42c966d801bf2876efa2bc2690b25398024cf
niclasdsgmbh <108051793+niclasdsgmbh@users.noreply.github.com>
2022-11-01 14:46:32 +0100

    patch

--- setup_manager.sh
+++ setup_manager.sh
@@ -97,10 +97,47 @@ sudo mv calicoctl /usr/local/bin/
 export DATASTORE_TYPE=kubernetes
 export KUBECONFIG=~/.kube/config
 
-calicoctl create -f ./files/calico_ippool_1.yml
-calicoctl create -f ./files/calico_ippool_2.yml
-calicoctl create -f ./files/calico_ippool_3.yml
+calicoctl --allow-version-mismatch create -f ./files/calico_ippool_1.yml
+calicoctl --allow-version-mismatch create -f ./files/calico_ippool_2.yml
+calicoctl --allow-version-mismatch create -f ./files/calico_ippool_3.yml
 
+openssl req -newkey rsa:4096 \
+           -keyout cni.key \
+           -nodes \
+           -out cni.csr \
+           -subj "/CN=calico-cni"
+
+sudo openssl x509 -req -in cni.csr \
+                  -CA /etc/kubernetes/pki/ca.crt \
+                  -CAkey /etc/kubernetes/pki/ca.key \
+                  -CAcreateserial \
+                  -out cni.crt \
+                  -days 365
+
+sudo chown $(id -u):$(id -g) cni.crt
+
+APISERVER=$(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
+
+kubectl config set-cluster kubernetes \
+    --certificate-authority=/etc/kubernetes/pki/ca.crt \
+    --embed-certs=true \
+    --server=$APISERVER \
+    --kubeconfig=cni.kubeconfig
+
+kubectl config set-credentials calico-cni \
+    --client-certificate=cni.crt \
+    --client-key=cni.key \
+    --embed-certs=true \
+    --kubeconfig=cni.kubeconfig
+
+kubectl config set-context default \
+    --cluster=kubernetes \
+    --user=calico-cni \
+    --kubeconfig=cni.kubeconfig
+
+kubectl config use-context default --kubeconfig=cni.kubeconfig
+
+kubectl apply -f ./files/calico_cluster_role.yml
 ##############################
 # MinIO                      #
 ##############################
