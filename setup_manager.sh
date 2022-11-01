307bf5911f4794b97146b833a4b050bdbe91a07b
niclasdsgmbh <108051793+niclasdsgmbh@users.noreply.github.com>
2022-11-01 02:27:42 +0100

    Update setup_manager.sh

--- setup_manager.sh
+++ setup_manager.sh
@@ -83,7 +83,7 @@ apt-mark hold \
 # CONFIG KUBERNETES          #
 ##############################
 
-kubeadm init --pod-network-cidr=192.168.0.0/16 --control-plane-endpoint=manager-1
+kubeadm init --pod-network-cidr=10.0.0.0/16 --control-plane-endpoint=manager-1
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
