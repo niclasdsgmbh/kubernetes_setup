aaf7a47c5784cee0191b734bab36a11e0d3aac70
niclasdsgmbh <108051793+niclasdsgmbh@users.noreply.github.com>
2022-11-01 02:03:08 +0100

    Update setup_manager.sh

--- setup_manager.sh
+++ setup_manager.sh
@@ -83,7 +83,7 @@ apt-mark hold \
 # CONFIG KUBERNETES          #
 ##############################
 
-kubeadm init --control-plane-endpoint=manager-1
+kubeadm init --pod-network-cidr=192.168.0.0/16 --control-plane-endpoint=manager-1
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
@@ -139,9 +139,16 @@ docker run -d \
     portainer/portainer-ee:latest
  
 ##############################
-# APPLY YAMLS                #
+# DEPLOY CALICO CNI          #
 ##############################
 
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 kubectl apply -f https://downloads.portainer.io/ee2-16/portainer-agent-k8s-nodeport.yaml
+
+##############################
+# CREATE TOKEN               #
+##############################
+
+kubeadm token create --print-join-command
+
 
