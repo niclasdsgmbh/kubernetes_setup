141d8c1af3195294d9e2f0e2af8d57903447076d
niclasdsgmbh <108051793+niclasdsgmbh@users.noreply.github.com>
2022-11-06 20:41:19 +0100

    Update init_cluster.sh

--- init_cluster.sh
+++ init_cluster.sh
@@ -13,8 +13,10 @@
     cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     chown $(id -u):$(id -g) $HOME/.kube/config
 
+    kubectl apply -f https://raw.githubusercontent.com/cilium/cilium/v1.6.5/install/kubernetes/quick-install.yaml
+
+
+
     kubectl apply -f https://raw.githubusercontent.com/minio/docs/master/source/extra/examples/minio-dev.yaml
     
-    
-    kubectl apply -f https://raw.githubusercontent.com/cilium/cilium/v1.6.5/install/kubernetes/quick-install.yaml
     kubectl apply -f https://raw.githubusercontent.com/portainer/k8s/master/deploy/manifests/portainer/portainer-lb-ee.yaml
